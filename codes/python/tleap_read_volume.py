cc4c25fdf959dac04485120bcc67bf90e89a9e12
agrero <86486604+agrero@users.noreply.github.com>
2023-08-17 10:06:14 -0700

    added annotations

--- codes/python/tleap_read_volume.py
+++ codes/python/tleap_read_volume.py
@@ -1,19 +1,27 @@
 import argparse
 import os 
 
+# arguemnt parser for handling directories and input files
+
 parser = argparse.ArgumentParser(
     prog='tleap solvate read',
     description='reads tleap out files for the box volume'
 )
+# files
 
 parser.add_argument('-c', '--config', default='gen_sim_config.in')
 parser.add_argument('-to', '--tleap_out', default='tleap.out')
+
+# paths/directories
 
 parser.add_argument('-o', '--start_dir', default='start')
 parser.add_argument('-cond', '--config_dir', default='test')
 parser.add_argument('-p', '--pdb_path')
 
 args = parser.parse_args()
+
+# open/create the config dictionary
+## to be functionalized
 
 config_dict = {}
 try:
@@ -30,9 +38,11 @@ try:
 except:
     raise FileNotFoundError('File could not be found')
 
+# list out ion concentrations and types from the config file
 ion_concentrations = config_dict['ion concentrations']
 ion_types = config_dict['ion types (same order)']
 
+# format ion components based on how many their are
 if type(ion_types) != list:
     ion_types = ion_types.strip(' ').strip('()').split(',')
     print(ion_types)
@@ -40,35 +50,46 @@ else:
     ion_types = [ion.strip(' ').strip('()').split(',') for ion in ion_types]
     print(ion_types)
 
-
+# get volume from previous step
 with open(args.tleap_out, 'r') as f:
     volume_text = [i for i in f.read().split('\n') if 'Volume' in i]
     vol_comp = volume_text[0].split(' ')
     volume = float(vol_comp[3])
-
+# calculate volume in litres
 volume_l = volume / (((10**10)**3)) * ((10**2)**3) / (10**3)
 
+# format ion concentrations into tleap commands 
 
-print(f'types orig: {ion_types}\n')
-print(f'ion_concentrations: {ion_concentrations}')
+no_ions_lines = [] # repo for tleap commands to write 
 
-no_ions_lines = []
+# protocol for multiple ionic compounds
 
 if type(ion_concentrations) == list:
+
+    # calculate the number of ions based on the volume and desired concentration
 
     for ndx, conc in enumerate(ion_concentrations):
         no_atoms = int(conc) / (10 ** 3) * (6.022 * 10 ** 23)
         no_ions = (no_atoms * volume_l)
-        # need to zip the two together
+
+        # special checks for divalent cations (currently only Mg)
+        ## just have a constants file where all the di/tri/quad(?)valent ions are in a list (see truth testing later)
 
         if ion_types[ndx][0] == 'Mg': # be more clever later
             line = f'addIonsRand system {ion_types[ndx][0]} {round(no_ions)} {ion_types[ndx][1]} {2*round(no_ions)}'    
+
         else:
             line = f'addIonsRand system {ion_types[ndx][0]} {round(no_ions)} {ion_types[ndx][1]} {round(no_ions)}'
+
         no_ions_lines.append(line)
+
+## this should be the else but if you cant tell i was rushing
 
 elif ion_concentrations == '':
     pass
+
+# protocol for singular compounds
+
 else:
     no_atoms = int(ion_concentrations) / (10 ** 3) * (6.022 * 10 ** 23)
     no_ions = (no_atoms * volume_l)
@@ -78,24 +99,33 @@ else:
     else:
         line = f'addIonsRand system {ion_types[0]} {round(no_ions)} {ion_types[1]} {round(no_ions)}'
 
+# write the final tleap configuration file
 
 with open(os.path.join(args.config_dir, 'tleap.in'), 'w') as f:
+
+    # input force field data
 
     f.write('source leaprc.protein.ff14SB\nsource leaprc.water.tip3p\n')
 
     if config_dict['contains dna (boolean)'] in ['True', 'T', 'true', 't']:
         f.write('source leaprc.DNA.OL21\n')
+
+    # loading the pdb as system and solvating
+
     f.write(f"""system = loadpdb {args.pdb_path}
 solvateBox system TIP3PBOX 14 iso\n""")
     
+    # playing with ions
+
     if config_dict['neutralize system (boolean)'] in ['True', 't', 'T', 'true']:
         f.write('addIonsRand system Na+ 0\n')
 
     for line in no_ions_lines:
         f.write(f'{line}\n')
 
-    # change this to take directories from the contig file
+    ## change this to take directories from the contig file
     ## this is very long, tleap must have a way to make this closer together
+
     f.write(
         f"""savePDB system {os.path.join(args.start_dir, f"amber_{config_dict['run_name (string)']}")}
 saveAmberParm system {os.path.join(args.start_dir, f"{config_dict['run_name (string)']}_solv.parm7")} {os.path.join(args.start_dir, f"{config_dict['run_name (string)']}_solv.rst7")}
--- codes/tleap/tleap_solvate.py
+++ codes/tleap/tleap_solvate.py
@@ -1,5 +1,6 @@
 import argparse
 
+# arguemnt parser for intaking the pdb in order to solvate
 parser = argparse.ArgumentParser(
     prog='tleap solvate',
     description='writes a tleap.in file to solvate a given pdb'
@@ -8,9 +9,10 @@ parser = argparse.ArgumentParser(
 parser.add_argument('-p', '--pdb')
 parser.add_argument('-c', '--config_dir')
 
-
 args = parser.parse_args()
 
+
+# the text that will be used to get the volume of the box
 text = f"""source leaprc.protein.ff14SB
 
 source leaprc.water.tip3p
@@ -20,5 +22,6 @@ system = loadpdb {args.pdb}
 solvateBox system TIP3PBOX 14 iso
 """
 
+# write initial tleap input file
 with open(f'{args.config_dir}/solvate_tleap.in', 'w') as f:
     f.write(text)
