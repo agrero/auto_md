b4800e37bca4e14c3d24678693dfd7cc224e2617
agrero <thegrero@gmail.com>
2023-09-11 15:29:04 -0700

    reformatting

--- le4pd_input_generation.py
+++ le4pd_input_generation.py
@@ -19,19 +19,24 @@ config_dict = mdf.read_sysconfig(args.config)
 #### Defining Directories ####
 
 # current working directory
+
 working_dir = os.getcwd()
 protname = config_dict['run name']
 
 # start_directory is directory to parm7 file with file name included
+
 topology = os.path.join(working_dir, protname, 'start', f"{protname}_solv.parm7") 
 
 # pdb directory is directory to pdb file with file name included
+
 pdb_directory= os.path.join(config_dict['pdb directory'], config_dict['pdb name'])
 
 # directory containing analysis script material
+
 analysis_directory = os.path.join(config_dict['auto_md directory'], 'codes', 'analysis')
 
 # path to the headers
+
 header_path = os.path.join(config_dict['auto_md directory'], 'codes', 'headers', 'slurm.head')
 
 
@@ -40,6 +45,7 @@ end = int(args.final)
 total_time = (end - start + 1)*5
 
 ## i a m still not a fan of a lot of these variable names
+
 traj = f"combine_traj_{start}_{end}"
 pdb = f"{protname}_solvated.pdb" # this is a pdb
 rem_rotation = "remove_rotation"
@@ -48,6 +54,7 @@ new_structure = f"{rem_rotation}/structure.pdb"
 # Write Initial Bash Script
 
 # analysis setup script
+
 with open(os.path.join(analysis_directory, 'analysis.sh'), 'r') as f:
     analysis_setup = f.read().format(
         initial = start,
@@ -60,6 +67,7 @@ with open(os.path.join(analysis_directory, 'analysis.sh'), 'r') as f:
 
 ## should make filling out a header a function
 # slurm header
+
 with open(header_path, 'r') as f:
     analysis_header = f.read().format(
         md_type = 'analysis',
@@ -74,7 +82,8 @@ with open(header_path, 'r') as f:
     )
 
 # write script
-with open('run_combinetraj.sh', 'w') as w:
+
+with open(os.path.join(os.getcwd(),'run_combinetraj.sh'), 'w') as w:
     w.write(f'{analysis_header}\n{analysis_setup}')
     
 # Write cpptraj command list
@@ -90,17 +99,18 @@ trajout {start}-{end}_{protname}_prod{total_time}ns.nc
 
 go"""
 
-with open('combinetraj_test.cpptraj', 'w') as w:
+with open(os.path.join(os.getcwd(),'combinetraj_test.cpptraj'), 'w') as w:
+
     w.write(combine_traj_text)
 
 #Write initial tcl_script
-
+## i dont like how long this is
 write_trr_text = f"""mol new {topology}
 animate read netcdf combinetraj_{args.initial}_{args.final}/{args.initial}-{args.final}_{protname}_prod{total_time}ns.nc waitfor all 0
 animate write trr trajectory.trr waitfor all 0 
 quit"""
 
-with open('write_trr_test.tcl', 'w') as w:
+with open(os.path.join(os.getcwd(), 'write_trr_test.tcl'), 'w') as w:
     w.write(write_trr_text)
     
 # Write process_1.sh
@@ -117,7 +127,7 @@ with open(os.path.join(analysis_directory, 'le4pd_setup.sh'), 'r') as f:
     )
 
 # writing script
-with open('process_1.sh', 'w') as f:
+with open(os.path.join(os.getcwd(), 'process_1.sh'), 'w') as f:
     f.write(f'{analysis_header}\n{le4pd_setup}')
 
 #Execute bash script
